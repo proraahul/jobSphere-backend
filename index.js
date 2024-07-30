04bfa124fbe7aea9934aaeb25adc2a72ea8169cb
Rahul saini <rs7965498@gmail.com>
2024-07-30 15:57:44 +0530

    jobSphere job portal backend

---
+++ index.js
@@ -0,0 +1,39 @@
+import express, { urlencoded } from "express";
+import connectDB from "./db/connection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+const BASE_URL = process.env.BASE_URL;
+
+dotenv.config();
+// connect db
+connectDB();
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin: BASE_URL,
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+// api's route
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.listen(PORT, () => {
+    console.log(`server running at port ${PORT}`);
+});
