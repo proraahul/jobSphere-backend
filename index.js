a4dc7c5e4793d431e991fe1dda0d51f5ddc7784a
Rahul saini <rs7965498@gmail.com>
2024-07-30 22:06:26 +0530

    fix index

--- index.js
+++ index.js
@@ -22,18 +22,19 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
-// app.use(cors({
-//     origin: BASE_URL,
-//     credentials: true
-// }));
+app.use(cors({
+    origin: "https://jobsphere-backend.onrender.com",
+    credentials: true
+}));
 // console.log(`Allowed CORS origin: ${BASE_URL}`);
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', BASE_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', "https://jobsphere-backend.onrender.com");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
   
 
 // api's route
