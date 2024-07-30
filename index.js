1584d84f1abfb1fa1630791270f209c6c0c443c8
Rahul saini <rs7965498@gmail.com>
2024-07-30 21:58:19 +0530

    add cors

--- index.js
+++ index.js
@@ -22,11 +22,19 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin: BASE_URL,
-    credentials:true
-}
-app.use(cors(corsOptions));
+// app.use(cors({
+//     origin: BASE_URL,
+//     credentials: true
+// }));
+// console.log(`Allowed CORS origin: ${BASE_URL}`);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', BASE_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
 
 // api's route
 app.use("/api/v1/user", userRoute);
