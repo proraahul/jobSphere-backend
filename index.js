377f1aa38e358f43af3693d38b2c1d2cd035ba01
Rahul saini <rs7965498@gmail.com>
2024-07-31 09:51:59 +0530

    add cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-const BASE_URL = process.env.BASE_URL;
+// const BASE_URL = process.env.BASE_URL;
 
 dotenv.config();
 // connect db
@@ -22,19 +22,27 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin: "https://jobsphere-backend.onrender.com",
-    credentials: true
-}));
+// app.use(cors({
+//     origin: "https://jobsphere-backend.onrender.com",
+//     credentials: true
+// }));
 // console.log(`Allowed CORS origin: ${BASE_URL}`);
+const allowedOrigins = ['https://jobsphere-backend.onrender.com"'];
 
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', "https://jobsphere-backend.onrender.com");
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-//   });
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
   
 
 // api's route
